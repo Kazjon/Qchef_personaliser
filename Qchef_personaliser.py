745e2c53a1c20497299f9d251da0f584e20b6ba4
otayeby <otayeby@gmail.com>
2018-07-26 21:29:49 -0400

    Got predictions and their accuracies. Also, added class finalizer for the random forest classifier and played with the different settings for epochs and number of batches for the neural network.

--- Qchef_personaliser.py
+++ Qchef_personaliser.py
@@ -1,6 +1,7 @@
 import os, csv, sys, json, numpy as np, pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.layers.merge import Concatenate
@@ -8,37 +9,72 @@ from keras.losses import mean_squared_error
 from keras.backend import round as keras_round
 from user_input_training.survey_reader import survey_reader
 
-def simpleRatingPredictor(train_xset, train_raw_ys, test_xset, test_raw_ys):
+def RF_classifier(train_xset, train_raw_ys, test_xset, test_raw_ys):
 	# Initialize 4 RF predictors
 	predictors_list = [RandomForestClassifier() for i in range(4)]
 	# Create 1/0 arrays; 1: if > i, 0: if < i, where i is the class from 1 to 4
-	train_ys_list = [[int(y > (i+1) / 5.0) for y in train_raw_ys] for i in range(4)]
-	test_ys_list = [[int(y > (i+1) / 5.0) for y in test_raw_ys] for i in range(4)]
+	train_ys_list = [[int(y > (i+1)) for y in train_raw_ys] for i in range(4)]
+	test_ys_list = [[int(y > (i+1)) for y in test_raw_ys] for i in range(4)]
+	# train_ys_list = [[int(y > (i+1) / 5.0) for y in train_raw_ys] for i in range(4)]
+	# test_ys_list = [[int(y > (i+1) / 5.0) for y in test_raw_ys] for i in range(4)]
+	class_predictions_dict = {}
 	# Iterate over the 4 predictors and fit them to training then test them
-	for class_idx, (predictor, train_yset, test_yset) in enumerate(zip(predictors_list,train_ys_list,test_ys_list)):
+	for class_idx, (predictor, train_yset, test_yset) in enumerate(zip(predictors_list, train_ys_list, test_ys_list)):
 		# Fit the predictor
-		predictor.fit(train_xset,train_yset)
+		predictor.fit(train_xset, train_yset)
 		# Evaluate the predictor
-		print 'Predictor of class > ', class_idx + 1, ':', predictor.score(test_xset,test_yset)
+		predictor_score = predictor.score(test_xset, test_yset)
+		print 'Predictors accuracy of class > ', class_idx + 1, ':', predictor_score
+		class_prediction = predictor.predict(test_xset)
+		# print 'Predictions of class > ', class_idx + 1, ':', class_prediction
+		# Store class prediction
+		class_predictions_dict[class_idx + 1] = class_prediction
+	# Get final class prediction
+	final_class_predictions = [0] * len(class_predictions_dict[1])
+	for each_class in class_predictions_dict:
+		final_class_predictions += class_predictions_dict[each_class]
+	# print 'final_class_predictions', len(final_class_predictions), final_class_predictions
+	# Calculate accuracy
+	predictor_accuracy, predictor_accuracy_norm = accuracy_score(test_raw_ys, final_class_predictions, normalize=False), accuracy_score(test_raw_ys, final_class_predictions)
+	print 'predictor_accuracy', predictor_accuracy, predictor_accuracy_norm * 100, '%'
+	predictor_recall_fscore = precision_recall_fscore_support(test_raw_ys, final_class_predictions, average='macro')
+	print 'predictor_recall_fscore', predictor_recall_fscore
 
-def neuralRatingPredictor(train_xset, train_yset, test_xset, test_raw_ys):
+def neuralRatingPredictor(train_xset, train_yset, test_xset, test_yset):
 	# Initialize a sequential model
 	model = Sequential()
 	# Create a Relu layer with the number of predictive variables
-	model.add(Dense(64, activation="relu", input_dim=num_predictive_vars))
+	model.add(Dense(64, activation='relu', input_dim=num_predictive_vars))
 	# Add dropout layer
 	model.add(Dropout(0.5))
 	# Add a linear layer
-	model.add(Dense(1, activation="linear"))
+	model.add(Dense(1, activation='linear'))
 	# Compile with MSE as the loss measure, use RMSprop as the optimizer
-	model.compile(optimizer="rmsprop", loss="mse", metrics=["accuracy"])
+	model.compile(optimizer='rmsprop', loss='mse', metrics=['accuracy', 'categorical_accuracy'])
+	# Determine the num_epochs and num_batches
+	num_batches = 20
+	num_epochs = 10000
+	batch_size = len(train_xset) / num_batches
 	# Fit the training into the model
-	model.fit(np.array(train_xset, dtype=np.float32),np.array(train_yset, dtype=np.float32),epochs=100,batch_size=100)
+	# model.fit(np.array(train_xset, dtype=np.float32), np.array(train_yset, dtype=np.float32), epochs=num_epochs, batch_size=100)
+	# model.fit(np.array(train_xset, dtype=np.float32), np.array(train_yset, dtype=np.float32), epochs=num_epochs, batch_size=len(test_xset))
+	# model.fit(np.array(train_xset, dtype=np.float32), np.array(train_yset, dtype=np.float32), epochs=num_epochs, batch_size=len(train_xset))
+	model.fit(np.array(train_xset, dtype=np.float32), np.array(train_yset, dtype=np.float32), epochs=num_epochs, batch_size=batch_size)
 	# Evaluate the model using the test dataset
-	# model.evaluate(np.array(test_xset, dtype=np.float32),np.array(test_yset, dtype=np.float32))
-	model.evaluate(np.array(test_xset, dtype=np.float32),np.array(test_raw_ys, dtype=np.float32))
+	print 'Evaluation:'
+	model.evaluate(np.array(test_xset, dtype=np.float32), np.array(test_yset, dtype=np.float32))
+	# model.evaluate(np.array(test_xset, dtype=np.float32), np.array(test_raw_ys, dtype=np.float32))
+	# Predict the output of the test dataset
+	test_pred = model.predict(np.array(test_xset, dtype=np.float32), batch_size=len(test_xset))
+	# print 'test_pred', type(test_pred), test_pred
+	test_pred_rounded = np.around(test_pred)
+	# print 'rounding the predictions:', set(test_pred_rounded.flat), test_pred_rounded
+	predictor_accuracy, predictor_accuracy_norm = accuracy_score(test_yset, test_pred_rounded, normalize=False), accuracy_score(test_yset, test_pred_rounded)
+	print 'predictor_accuracy', predictor_accuracy, predictor_accuracy_norm * 100, '%'
+	predictor_recall_fscore = precision_recall_fscore_support(test_yset, test_pred_rounded, average='macro')
+	print 'predictor_recall_fscore', predictor_recall_fscore
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	# Get the argument variables
 	user_input_train_fn = sys.argv[1]
 	mode = sys.argv[2]
@@ -53,7 +89,7 @@ if __name__ == "__main__":
 	# print users_fam_dir_cols, users_cuisine_pref_cols, users_knowledge_cols, users_surp_ratings_cols, users_surp_pref_cols
 	# Read the prepared user input
 	user_input_train_df = pd.read_csv(user_input_train_fn)
-	# Select users or recipes
+	# Divide users or recipes
 	# Remove user and recipe IDs
 	user_input_train_df.drop(['Recipe ID', 'User ID'], axis=1, inplace=True)
 	# Remove unwanted features; choose to drop any of the following: users_fam_dir_cols, users_cuisine_pref_cols, users_knowledge_cols, users_surp_pref_cols
@@ -62,7 +98,14 @@ if __name__ == "__main__":
 	print 'Current used features:', user_input_train_df.columns
 	# Get the target variable
 	target_var = user_input_train_df['users_surp_ratings']
-	all_ys = target_var.values
+	all_ys = []
+	# Choose and build the model
+	if mode == 'RF_classifier':
+		all_ys = target_var.values * 5
+	elif mode == 'RF_regressor':
+		all_ys = target_var.values
+	elif mode == 'neural':
+		all_ys = target_var.values * 5
 	# Drop the target variable
 	user_input_train_df.drop(['users_surp_ratings'], axis=1, inplace=True)
 	# Get predictor variables
@@ -80,9 +123,12 @@ if __name__ == "__main__":
 		# Split the training from testing using the IDs
 		train_xs, test_xs = all_xs[train_ids], all_xs[test_ids]
 		train_ys, test_ys = all_ys[train_ids], all_ys[test_ids]
+		# print 'test_ys', len(test_ys), set(test_ys), test_ys
 		# Choose and build the model
-		if mode == "simple":
-			simpleRatingPredictor(train_xs, train_ys, test_xs, test_ys)
-		elif mode == "neural":
+		if mode == 'RF_classifier':
+			RF_classifier(train_xs, train_ys, test_xs, test_ys)
+		elif mode == 'RF_regressor':
+			RF_regressor(train_xs, train_ys, test_xs, test_ys)
+		elif mode == 'neural':
 			neuralRatingPredictor(train_xs, train_ys, test_xs, test_ys)
-			# break
+		break
